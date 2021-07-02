62bee0233f0a7ea399852b6680233fe4d3771575
Anat Demb <anatdemb@Anats-MacBook-Air.local>
2021-07-02 14:24:06 -0400

    Updates, adding more code and plots

--- LASSO.R
+++ LASSO.R
@@ -27,11 +27,24 @@ plot(cv.lasso)
 ## two dashed lines: left line points to the lambda with the lowest mean squared error 
 ## the right line points to the number of variables with the highest deviance within 1 standard deviation
 
-# To see the best lambda value 
+# To see the best crossed-validated lambda value 
 cv.lasso$lambda.min
+
+# Fit final model
 cat('Min Lambda:', cv.lasso$lambda.min, '\n 1Sd Lambda:', cv.lasso$lambda.1se)
 df_coef<- round(as.matrix(coef(cv.lasso,s=cv.lasso$lambda.min)),2)
 
 # To see all the variables LASSO considered important
 # High positive or low negative means this variable is more important
 df_coef[df_coef[,1] !=0,]
+
+# To see how increasing lambda shrinks the coefficients 
+# Each line shows coefficients for one variable (for different lambdas)
+# The higher the lambda- the closer the coefficients are to zero 
+lambdas_to_try<- 10^seq(-3,5,length.out=100)
+res<- glmnet(x,y,
+             alpha=1,
+             lamba=lambdas_to_try,
+             standardize = FALSE)
+plot(res,xvar="lambda")
+legend("bottomright",lwd=1,col=1:6,legend=colnames(x), cex=.7)
